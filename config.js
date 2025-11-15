62433ad24ffa3cdc5818061b2fb776bd9564e255
joker09245 <jokergamerjokergamer0@gmail.com>
2025-11-15 23:08:26 +0530

    Config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    token: "YOUR_BOT_TOKEN_HERE",
+    prefix: "!",
+    owners: ["YOUR_USER_ID"],
+    
+    // Emojis in config
+    emoji: {
+        MONEY: '💰',
+        DANCE: '💃',
+        PAID: '💸'
+    },
+    
+    // Other config
+    color: "#FF0000",
+    supportServer: "https://discord.gg/yourserver"
+};
